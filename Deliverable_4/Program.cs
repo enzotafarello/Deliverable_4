fc4b8dca08490156bb9b5794c89b1cc406ff2ae6
Enzot <Enzot@DESKTOP-3QSBHAL>
2023-02-08 23:12:06 -0500

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Author: Enzo Tafarello Negrao
+Date:2/9/23
+Description: C# console application that computes the first 25 numbers of the Fibonacci sequence */
+
+using System;
+
+namespace Deliverable_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacci_n1 = 0;
+            int fibonacci_n2 = 1;
+            int fibonacci_n3;
+            int i;
+
+            int[] series = new int[25];
+
+            series[0] = fibonacci_n1;
+            series[1] = fibonacci_n2;
+
+            for(i=2; i<25;++i)
+            {
+                fibonacci_n3 = fibonacci_n1 + fibonacci_n2;
+                series[i] = fibonacci_n3;
+                fibonacci_n1 = fibonacci_n2;
+                fibonacci_n2 = fibonacci_n3;
+            }
+            for (i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + series[i]);
+            }
+            
+
+        }
+    }
+}
